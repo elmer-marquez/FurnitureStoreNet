a5d279168a72cc3c6cbf69f2f79b4a35ba9c3b66
Elmer Marquez <elmer.marquez.la@gmail.com>
2024-03-12 20:46:40 -0600

    se agrego el controlador Products

---
+++ FurnitureStore.Api/Controllers/ProductsController.cs
@@ -0,0 +1,87 @@
+﻿using FurnitureStore.Data;
+using FurnitureStore.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ProductsController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p.Id == id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            if(product == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new {id = product.Id}, product);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Product product)
+        {
+            if( product == null)
+            {
+                return BadRequest();
+            }
+
+            var pdUpdate = await _context.Products.FirstOrDefaultAsync(pd=>pd.Id == product.Id);
+            if(pdUpdate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Update(pdUpdate);
+            await _context.SaveChangesAsync();  
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(pd=>pd.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
